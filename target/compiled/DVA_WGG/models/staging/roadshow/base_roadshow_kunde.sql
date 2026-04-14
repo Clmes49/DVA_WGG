5c256e8e366aa5cec58b983b2097151e1c3ed0e0
Walid El Otmani <walid.el-otmani@stud.hs-hannover.de>
2026-04-14 16:26:35 +0200

    Staging der Kunden Tabelle aus Roadshow

---
+++ target/compiled/DVA_WGG/models/staging/roadshow/base_roadshow_kunde.sql
@@ -0,0 +1,17 @@
+
+
+select
+    kundeid,
+    vereinspartnerid,
+    vorname,
+    name,
+    geschlecht,
+    geburtsdatum,
+    telefon,
+    mobil,
+    email,
+    kreditkarte,
+    gueltigbis,
+    kkfirma,
+    current_timestamp as ldts
+from "postgres"."roadshow"."kunde"
