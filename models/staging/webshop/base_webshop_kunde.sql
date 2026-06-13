d338c0c70512e5c37cc50773e3437908ec72309d
WalidOps <waliduuelotmani@gmail.com>
2026-06-13 14:35:14 +0200

    base_webshop_kunde.sql kommentiert

--- models/staging/webshop/base_webshop_kunde.sql
+++ models/staging/webshop/base_webshop_kunde.sql
@@ -1,7 +1,13 @@
-{{ config(materialized='view') }}
+-- Base Model: base_webshop_kunde
+-- Schicht: Staging, Stufe 1 von 2
+-- Quelle: webshop.kunde
+-- Zweck: Rohdaten-Selektion aus der Quelltabelle und Hinzufügen des Load Date Timestamps. Keine Transformation, nur Spaltenauswahl
+
+
+{{ config(materialized='view') }} -- Materialisierung als View gesetzt
 
 select
-    kundeid,
+    kundeid, 
     vereinspartnerid,
     vorname,
     name,
@@ -13,5 +19,7 @@ select
     kreditkarte,
     gueltigbis,
     kkfirma,
-    cast('{{var("batch_ldts")}}' as timestamptz) as ldts
-from {{ source('webshop', 'kunde') }}
+    cast('{{var("batch_ldts")}}' as timestamptz) as ldts -- Ladezeitpunkt wird dynamisch per CLI übergeben
+from {{ source('webshop', 'kunde') }} -- Referenz aus src_willibald.yml
+
+
